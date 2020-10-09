b30bffacb06fcd7bc69143b0d7228a564d6c27f4
Jarrek Holmes <jarrek.holmes@gusto.com>
2020-10-09 12:53:26 -0600

    Initial starter code

--- src/App.test.js
+++ src/App.test.js
@@ -10,42 +10,19 @@ const render = () => r(<App />);
 describe("App", () => {
   it("renders the card header", () => {
     render();
-    expect(screen.getByText("My favorite animal")).toBeInTheDocument();
   });
 
   it("render the default favorite animal", () => {
     render();
-    expect(screen.getByText("I love Cats")).toBeInTheDocument();
   });
 
   it("render the edit button", () => {
     render();
-    expect(screen.getByText("Edit")).toBeInTheDocument();
   });
 
   it("open the animal form when the edit button is clicked", () => {
     render();
-    const editButton = screen.getByRole("button");
-    userEvent.click(editButton);
-
-    expect(screen.getByText("Save")).toBeInTheDocument();
-    expect(screen.queryByText("Edit")).not.toBeInTheDocument();
   });
 
-  it("fetches an animal fact from axios", async () => {
-    const axiosMock = jest
-      .spyOn(axios, "get")
-      .mockResolvedValue({ data: { text: "Jarrek loves dogs and cats" } });
-    const url = "https://cat-fact.herokuapp.com/facts/random?animal_type=dog";
-
-    render();
-
-    await waitFor(() => {
-      expect(axiosMock).toHaveBeenCalled();
-      expect(axiosMock).toHaveBeenCalledWith(url);
-      expect(
-        screen.getByText("Cats fact: Jarrek loves dogs and cats")
-      ).toBeInTheDocument();
-    });
-  });
+  it("fetches an animal fact from axios", async () => {});
 });
